1d93a696ab6a828b569a3711956ef446a11bafb3
jhonatan cudris <weiselcudris@hotmail.com>
2019-07-05 17:17:05 -0500

    creando las relaciones entre modelos

---
+++ app/Buyer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+use App\Transaction;
+
+
+class Buyer extends User
+{
+    public function transaction(){
+
+        return $this->hasMany(Transaction::class);
+    }
+}
---
+++ app/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    const PRODUCTO_DISPONIBLE = 'disponible';
+    const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable=['name','description','quantity','status','image','seller_id'];
+
+
+    public function estaDisponible(){
+
+        return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
+}
+
+
